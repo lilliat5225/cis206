521a8940eebf158987b2e79a4ec3591877fbd8e4
lilliat5225 <120775588+lilliat5225@users.noreply.github.com>
2023-06-08 20:46:51 +0000

    update

--- Final/Mountain/Mountain.java
+++ Final/Mountain/Mountain.java
@@ -1,46 +1,48 @@
-/* 
- File: Mountain.java
- Description: Final
- Created: 06/08/2023
+/*
+File: Mountain.java
+Description: Final
+Created: 06/08/2023
 Author: Lillian Thacker
- mail: lilliat5225@student.vvc.edu.
+mail: lilliat5225@student.vvc.edu.
 */
+
+
 public class Mountain {
-    private String name;
-    private String country;
-    private double elevation;
+  private String name;
+  private String country;
+  private double elevation;
 
-    public Mountain(String name, String country, double elevation) {
-        this.name = name;
-        this.country = country;
-        this.elevation = elevation;
-    }
+  public Mountain(String name, String country, double elevation) {
+    this.name = name;
+    this.country = country;
+    this.elevation = elevation;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getCountry() {
-        return country;
-    }
+  public String getCountry() {
+    return country;
+  }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+  public void setCountry(String country) {
+    this.country = country;
+  }
 
-    public double getElevation() {
-        return elevation;
-    }
+  public double getElevation() {
+    return elevation;
+  }
 
-    public void setElevation(double elevation) {
-        this.elevation = elevation;
-    }
+  public void setElevation(double elevation) {
+    this.elevation = elevation;
+  }
 
-    public double toMeters() {
-        return elevation / 3.2808;
-    }
+  public double toMeters() {
+    return elevation / 3.2808;
+  }
 }
